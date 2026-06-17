dae7764b7c0918b8e037a9a2010a1f97ae79ba31
osamah-devops <osamah.devops@gmail.com>
2026-06-17 01:44:21 -0400

    Start Infra 2

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,3 +9,8 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet."
+  type        = string
+  default     = "10.0.1.0/24"
+}
