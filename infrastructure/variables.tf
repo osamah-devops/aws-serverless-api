79ea59319e55772f1a900d94fae11bbc31764e17
osamah-devops <osamah.devops@gmail.com>
2026-06-17 10:51:52 -0400

    Start Infra 2

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,16 +1,10 @@
 variable "region" {
-  description = "The AWS region to deploy resources in."
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
-
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC."
+variable "environment" {
+  description = "The deployment environment (e.g., dev, staging, prod)"
   type        = string
-  default     = "10.0.0.0/16"
-}
-variable "public_subnet_cidr" {
-  description = "The CIDR block for the public subnet."
-  type        = string
-  default     = "10.0.1.0/24"
+  default     = "dev"
 }
