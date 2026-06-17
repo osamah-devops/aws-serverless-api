6473b57a509c821836dd730e052e70a9d31ffba3
osamah-devops <osamah.devops@gmail.com>
2026-06-17 00:44:50 -0400

    Add Infrastructure

---
+++ infrastructure/version.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 6.0"
+        }
+    }
+    required_version = ">= 1.15"
+    backend "s3" {
+        bucket = "my-terraform-state-bucket"
+        key    = "project_1/terraform.tfstate"
+        region = "us-east-1"
+        use_lockfile = true
+    }
+}
